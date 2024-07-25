eff5fb09f90f6cbbb6d9cd0c6a46b0a4e5dcb4c5
Brian Carrillo <bcarrillo2302@gmail.com>
2024-07-25 19:09:52 +0200

    Save cookies

--- uploadIG_Selenium.py
+++ uploadIG_Selenium.py
@@ -1,4 +1,5 @@
 # pip3 install selenium
+# pip3 install pyautogui  
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -7,12 +8,33 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import pickle
+import os
+import pyautogui
 
 # Configuración del WebDriver
 chrome_options = Options()
 chrome_options.add_argument("--disable-notifications")
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+# Ruta del archivo de cookies
+cookies_file = 'instagram_cookies.pkl'
+
+# Función para cargar cookies
+def load_cookies(driver, cookies_file):
+    if os.path.exists(cookies_file):
+        driver.get("https://www.instagram.com")
+        cookies = pickle.load(open(cookies_file, "rb"))
+        for cookie in cookies:
+            driver.add_cookie(cookie)
+        driver.refresh()
+        driver.fullscreen_window()
+
+# Función para guardar cookies
+def save_cookies(driver, cookies_file):
+    cookies = driver.get_cookies()
+    pickle.dump(cookies, open(cookies_file, "wb"))
 
 # Abre Instagram
 driver.get("https://www.instagram.com")
@@ -21,16 +43,20 @@ driver.fullscreen_window()
 # Espera que la página cargue
 time.sleep(3)
 
-links = driver.find_elements("xpath", "//button")
-for link in links:
-    if "Rechazar cookies" in link.get_attribute("innerHTML"):
-        print("Rechazar cookies")
-        link.click()
-        break
+# Rechaza cookies si es necesario
+# links = driver.find_elements(By.XPATH, "//button")
+# for link in links:
+#     if "Rechazar cookies" in link.get_attribute("innerHTML"):
+#         print("Rechazar cookies")
+#         link.click()
+#         break
 
-time.sleep(5)
+# time.sleep(5)
 
-# Inicia sesión
+# Cargar cookies si existen
+load_cookies(driver, cookies_file)
+
+# Verifica si es necesario iniciar sesión
 try:
     username = driver.find_element(By.NAME, 'username')
     password = driver.find_element(By.NAME, 'password')
@@ -39,40 +65,53 @@ try:
     username.send_keys('someshortshots')
     password.send_keys('gk6PLvQyltFwQrTWd')
     password.send_keys(Keys.RETURN)
-except:
-    print("Sesión estaba iniciada")
 
-# Espera para que se realice el login
-time.sleep(5)
+    # Espera para que se realice el login
+    time.sleep(5)
 
-links = driver.find_elements("xpath", "//span")
+    # Guarda las cookies después de iniciar sesión
+    save_cookies(driver, cookies_file)
+except Exception as e:
+    print("Skip login")
+
+# Navega a la página de creación de contenido
+links = driver.find_elements(By.XPATH, "//span")
 for link in links:
     if "Crear" in link.get_attribute("innerHTML"):
         print("Crear")
         link.click()
         break
 
-
 time.sleep(5)
 
-# # Carga el archivo de video
-# upload_button = driver.find_element(By.XPATH, '//input[@type="file"]')
-# upload_button.send_keys('/path/to/tu_video.mp4')  # Cambia el path a tu video
+videopath = '/Users/briancarrillo/Descargas/instagram_reel.mp4'
+# Carga el archivo de video
+links = driver.find_elements(By.XPATH, "//button")
+for link in links:
+    if "Seleccionar del ordenador" in link.get_attribute("innerHTML"):
+        print("Seleccionar del ordenador")
+        link.click()
+        break
 
-# # Espera que el video se cargue
+time.sleep(2)  # Espera a que se abra el diálogo
+pyautogui.write(videopath)  # Escribe la ruta del archivo
+pyautogui.press('enter')
+pyautogui.press('enter')
+
+# Espera que el video se cargue
 # time.sleep(10)
 
-# # Introduce la descripción del Reel (opcional)
+# Introduce la descripción del Reel (opcional)
 # description_area = driver.find_element(By.XPATH, '//textarea[@aria-label="Descripción"]')
 # description_area.send_keys('Tu descripción del Reel')
 
-# # Publica el Reel
+# Publica el Reel
 # share_button = driver.find_element(By.XPATH, '//button[text()="Compartir"]')
 # share_button.click()
 
-# # Espera para que la publicación se realice
+# Espera para que la publicación se realice
 # time.sleep(5)
 
-# # Cierra el navegador
-time.sleep(60)
+# Cierra el navegador
+time.sleep(600)
 driver.quit()
