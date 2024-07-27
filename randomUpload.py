223bfe94c1d25a6b1297a210451dc69e3952ccad
Brian Carrillo <bcarrillo2302@gmail.com>
2024-07-27 21:07:55 +0200

    Embedded IG code

--- randomUpload.py
+++ randomUpload.py
@@ -1,6 +1,34 @@
+# pip3 install instagrapi Pillow moviepy python-dotenv
+
 import os
 import random
 import subprocess
+from dotenv import load_dotenv
+from instagrapi import Client
+
+
+
+# Cargar variables de entorno del archivo .env
+load_dotenv()
+# Obtener credenciales de las variables de entorno
+INSTAGRAM_USERNAME = os.getenv('INSTAGRAM_USERNAME')
+INSTAGRAM_PASSWORD = os.getenv('INSTAGRAM_PASSWORD')
+instagram_caption = """No problem! Here the information about Mercedes CLS GTR:
+
+The Mercedes CLS GTR is a remarkable racing car celebrated for its outstanding performance and sleek design. Powered by a potent 6.0-liter V12 engine, it delivers over 600 horsepower. 🔧
+
+Accelerated from 0-100 km/h takes approximately 3.7 seconds, with a remarkable top speed surpassing 320hm/h. 🥇
+
+Incorporating advanced aerodynamics features and cutting-edge stability technologies, the CLS GTR ensures exceptional stability and control, particularly high-speed maneuvers. 💨
+
+Originally priced around 1.5 million, the Mercedes CLS GTR is considered one of the most exclusive and prestigious racing cars ever produced. 💰
+
+Its limited production run of just five units adds to its rarity, making it highly sought after by racing enthusiasts and collectors worldwide. 🌎
+
+Join my Instagram, there are regular quizzes about cars and automotive news📱
+
+I invite you to my Telegram channel #fyp
+"""
 
 def GetRandomFile(relative_folder_path, extension=None):
     # Obtener la ruta absoluta de la carpeta
@@ -22,14 +50,22 @@ def GetRandomFile(relative_folder_path, extension=None):
     return random_file
 
 
-def RunUploader(script_path, file_path):
-    # Ejecutar el script con el archivo seleccionado como parámetro
-    result = subprocess.run(['python3', script_path, file_path], capture_output=True, text=True)
-    
-    # Imprimir la salida del script
-    print(result.stdout)
-    if result.stderr:
-        print(result.stderr)
+def UploadToInstagram(sourceVideo):
+
+    try:
+        cl = Client()
+        cl.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
+
+        # Ruta del video y configuración
+        video_path = sourceVideo
+
+        # Publicación del reel
+        cl.clip_upload(video_path, instagram_caption)
+
+        os.remove(sourceVideo)
+        print(f"Reel publicado con éxito. {sourceVideo}")
+    except:
+        print(f"Error al publicar el reel. {sourceVideo}")
     
 def main():
     # Obtener el directorio del script actual
@@ -43,7 +79,7 @@ def main():
         # Obtener un archivo al azar de la carpeta ./downloads/instagram
         random_file = GetRandomFile(instagram_folder, '.mp4')
         print(f"Archivo seleccionado para Instagram: {random_file}")
-        RunUploader(instagram_script, random_file)
+        UploadToInstagram(instagram_script, random_file)
     except Exception as e:
         print(f"Instagram error: {e}")
     
