f51fd959765cda2f6df43c95acbed5bc778f4a09
Brian Carrillo <bcarrillo2302@gmail.com>
2024-07-28 10:42:52 +0200

    Clean repo

--- runRandomUpload.sh
+++ runRandomUpload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /home/briancarrillo/SomeShortShots/downloaderEnv/bin/activate
+source /home/briancarrillo/SomeShortShots/downloaderEnv/bin/activate 
 python /home/briancarrillo/SomeShortShots/randomUpload.py
