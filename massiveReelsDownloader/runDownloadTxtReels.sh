102cfef5e76c875539b316e71ebd2f17136e58f8
Brian Carrillo <77851362+briancarrilloo@users.noreply.github.com>
2024-07-29 22:08:36 +0200

    Fix cd download txt (#10)

--- massiveReelsDownloader/runDownloadTxtReels.sh
+++ massiveReelsDownloader/runDownloadTxtReels.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Script para ejecutar downloadTxtReels.py accediendo al entorno de python automaticamente
 source /home/briancarrillo/SomeShortShots/downloaderEnv/bin/activate 
+cd /home/briancarrillo/SomeShortShots/massiveReelsDownloader/
 python /home/briancarrillo/SomeShortShots/massiveReelsDownloader/downloadTxtReels.py
