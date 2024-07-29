ab20e43b68a0791d2b74e25469ce1717087b3044
Brian Carrillo <bcarrillo2302@gmail.com>
2024-07-29 19:05:26 +0200

    Reorder files

--- uploaders/uploadTiktok.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Nombre del script: tu_script.sh
-
-echo "Script ejecutado en: $(date)" >> /home/briancarrillo/SomeShortShots/uploaders/play.log
-source /home/briancarrillo/SomeShortShots/downloaderEnv/bin/activate
-cd /home/briancarrillo/SomeShortShots/uploaders
-python3 play_Tiktok_Upload.py >> /home/briancarrillo/SomeShortShots/uploaders/play.log 2>&1
-echo "Script finalizado en: $(date)" >> /home/briancarrillo/SomeShortShots/uploaders/play.log
